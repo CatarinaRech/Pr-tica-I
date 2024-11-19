8e90ea37cd271296e0cad1906de5133ff4d8d7a0
CatarinaRech <161315081+CatarinaRech@users.noreply.github.com>
2024-11-19 09:45:57 -0300

    Add files via upload

--- create.php
+++ create.php
@@ -1,26 +1,43 @@
 <?php
-include 'db.php';
+include "db.php";
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $name = $_POST['name'];
+
+if($_SERVER ['REQUEST_METHOD'] == 'POST'){
+    $nome = $_POST ['nome'];
     $email = $_POST['email'];
+    $telefone = $_POST ['telefone'];
 
-    $sql = "INSERT INTO user (name, email) VALUES ('$name', '$email')";
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Novo registro criado com sucesso";
-    } else {
-        echo "Erro: " . $sql . "<br>" . $conn->error;
+    $sql = "INSERT INTO cliente (nome_cliente, email_cliente, telefone_cliente) VALUE ('$nome', '$email', '$telefone')";
+
+
+    if ($conn -> query($sql) === true) {
+        echo "Cadastro adicionado <br>" ;
+    } else{
+        echo "Erro $sql <br>". $conn -> error;
     }
 }
 
-$conn->close();
+
 ?>
 
-<form method="post" action="create.php">
-    Nome: <input type="text" name="name" required>
-    Email: <input type="email" name="email" required>
-    <input type="submit" value="Adicionar">
-</form>
 
-<a href="read.php">Ver registros.</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="create_cliente.php">
+        <label for="name" > Nome do Cliente:</label>
+        <input type="text" name="nome" require>
+        <label for="name" > Email do Cliente:</label>
+        <input type="email" name="email" require>
+        <label for="name" > Telefone do Cliente:</label>
+        <input type="text" name="telefone" require>
+        <input  type="submit" value="Cadastrar">
+    </form>
+</body>
+</html>
